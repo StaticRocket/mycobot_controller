4bd9aad5dc4f0bf92da58bc1d7ad8afe2bf688e8
Randolph Sapp <rs@ti.com>
2024-04-13 18:21:54 -0500

    console: rework the console so expand works
    Rework the elements such that the layout engine doesn't get confused
    with widgets attempting to expand vertically. Make the send button the
    same height as the text field.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- lib/console.dart
+++ lib/console.dart
@@ -21,45 +21,54 @@ class _ConsoleState extends State<Console> {
   final TextEditingController _controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return IntrinsicHeight(
-        child:
-            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
-      StreamBuilder(
-        stream: widget.channel.stream,
-        builder: (context, snapshot) {
-          widget.appendLog(snapshot.hasData ? '${snapshot.data}' : '');
-          return Expanded(
-              child: Container(
-                  child: ListView.builder(
-            reverse: true,
-            itemCount: widget.logList.length,
-            itemBuilder: (BuildContext context, int index) {
-              return Text('${widget.logList[index]}');
-            },
-          )));
-        },
-      ),
-      const SizedBox(height: 24),
-      Row(children: [
-        Expanded(
-          child: Form(
-            child: TextField(
-                controller: _controller,
-                decoration: const InputDecoration(
-                    border: OutlineInputBorder(), hintText: 'Send a command'),
-                onSubmitted: (value) {
-                  _sendMessage();
-                }),
-          ),
-        ),
-        IntrinsicHeight(
-          child: ElevatedButton(
-            onPressed: _sendMessage,
-            child: const Icon(Icons.send),
-          ),
-        ),
-      ]),
-    ]));
+    return Card(
+        child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  StreamBuilder(
+                    stream: widget.channel.stream,
+                    builder: (context, snapshot) {
+                      widget.appendLog(
+                          snapshot.hasData ? '${snapshot.data}' : '');
+                      return Flexible(
+                          child: Container(
+                              child: ListView.builder(
+                        reverse: true,
+                        itemCount: widget.logList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Text('${widget.logList[index]}');
+                        },
+                      )));
+                    },
+                  ),
+                  const SizedBox(height: 8),
+                  IntrinsicHeight(
+                    child: Row(children: [
+                      Flexible(
+                        child: Form(
+                          child: TextField(
+                              controller: _controller,
+                              decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  hintText: 'Send a command'),
+                              onSubmitted: (value) {
+                                _sendMessage();
+                              }),
+                        ),
+                      ),
+                      const SizedBox(width: 8),
+                      SizedBox.square(
+                        dimension: kTextTabBarHeight,
+                        child: IconButton.filled(
+                          onPressed: _sendMessage,
+                          icon: const Icon(Icons.send),
+                        ),
+                      ),
+                    ]),
+                  ),
+                ])));
   }
 
   void _sendMessage() {
