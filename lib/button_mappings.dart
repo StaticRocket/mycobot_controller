68af8152d53a87dff9d4b83549930b0555693d1c
Randolph Sapp <rs@ti.com>
2024-04-14 23:24:08 -0500

    button_mappings: switch to ElevatedButton.icon
    Switch these buttons out for ElevatedButton.icon so we can have some
    unambiguous text.
    
    Signed-off-by: Randolph Sapp <rs@ti.com>

--- lib/button_mappings.dart
+++ lib/button_mappings.dart
@@ -19,46 +19,63 @@ class _ButtonCardState extends State<ButtonCard> {
   @override
   Widget build(BuildContext context) {
     return Card(
-        child: Row(children: [
-      Column(children: [
-        PositionButton(
-            icon: const Icon(Icons.home),
-            sendMessage: () {
-              widget.sendMessage("G30");
-            }),
-        PositionButton(
-            icon: const Icon(Icons.flash_on),
-            sendMessage: () {
-              widget.sendMessage("M80");
-            }),
-        PositionButton(
-            icon: const Icon(Icons.flash_off),
-            sendMessage: () {
-              widget.sendMessage("M81");
-            }),
-        IconButton(
-          icon: const Icon(Icons.shuffle),
-          onPressed: _shuffle,
-        ),
-        IconButton(
-          icon: widget.demoBool
-              ? const Icon(Icons.pause)
-              : const Icon(Icons.play_arrow),
-          onPressed: () async {
-            await widget.toggleDemo();
-          },
-        ),
-      ])
-    ]));
+        child: Padding(
+            padding: const EdgeInsets.all(8),
+            child: Row(children: [
+              Column(children: [
+                PositionButton(
+                    icon: const Icon(Icons.home),
+                    label: const Text("Home"),
+                    sendMessage: () {
+                      widget.sendMessage("G30");
+                    }),
+                const SizedBox(height: 8),
+                PositionButton(
+                    icon: const Icon(Icons.flash_on),
+                    label: const Text("Power on motors"),
+                    sendMessage: () {
+                      widget.sendMessage("M80");
+                    }),
+                const SizedBox(height: 8),
+                PositionButton(
+                    icon: const Icon(Icons.flash_off),
+                    label: const Text("Power off motors"),
+                    sendMessage: () {
+                      widget.sendMessage("M81");
+                    }),
+                const SizedBox(height: 8),
+                ElevatedButton.icon(
+                  icon: const Icon(Icons.games),
+                  label: const Text("Rock, paper, scissors"),
+                  onPressed: _rockPaperSissors,
+                ),
+                const SizedBox(height: 8),
+                ElevatedButton.icon(
+                  icon: widget.demoBool
+                      ? const Icon(Icons.pause)
+                      : const Icon(Icons.play_arrow),
+                  label: const Text("Demo mode"),
+                  onPressed: () async {
+                    await widget.toggleDemo();
+                  },
+                ),
+              ])
+            ])));
   }
 
-  void _shuffle() {}
+  void _rockPaperSissors() {
+    //
+  }
 }
 
 class PositionButton extends StatefulWidget {
   const PositionButton(
-      {super.key, required this.icon, required this.sendMessage});
+      {super.key,
+      required this.icon,
+      required this.label,
+      required this.sendMessage});
   final Icon icon;
+  final Text label;
   final Function sendMessage;
 
   @override
@@ -68,9 +85,10 @@ class PositionButton extends StatefulWidget {
 class _PositionButtonState extends State<PositionButton> {
   @override
   Widget build(BuildContext context) {
-    return IconButton(
+    return ElevatedButton.icon(
       onPressed: _sendMessage,
       icon: widget.icon,
+      label: widget.label,
     );
   }
 
